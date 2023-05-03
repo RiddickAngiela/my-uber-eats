b0c1ccec75075437c7a685826003ab231bf18e6c
RiddickAngiela <angielariddick@gmail.com>
2023-05-03 11:34:38 +0300

    working on food jsx

--- src/componets/Food.jsx
+++ src/componets/Food.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import {data} from "../data/data"
 
 const Food = () => {
+    console.log(data)
   return (
     <div>
-        
+        <h1>Top Rated Menu Items</h1>
+
+        {/* Filter Row */}
+        <div>
+            {/* Filter Type */}
+            <div>
+
+            </div>
+            {/* Filter Price */}
+            <div></div>
+        </div>
     </div>
   )
 }
