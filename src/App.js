a2c6301eb58f309da7286d468e96c97b0a9ca7c8
RiddickAngiela <angielariddick@gmail.com>
2023-05-03 11:16:37 +0300

    Added the Hero component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
 import Navbar from "./componets/Navbar";
+import Hero from "./componets/Hero";
+import HeadlineCard from "./componets/HeadlineCard";
 
 function App() {
   return (
     <div>
      <Navbar />
+     <Hero />
+     <HeadlineCard />
     </div>
   );
 }
